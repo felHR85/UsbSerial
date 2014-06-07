c5796ac1bc4228906363bbd7912d2d999ab988b0
Felipe Herranz <felipe.herranz@gmail.com>
2014-06-07 13:20:51 +0200

    Open and set functions except flow control are completed

--- src/com/felhr/usbserial/PL2303SerialDevice.java
+++ src/com/felhr/usbserial/PL2303SerialDevice.java
@@ -17,7 +17,17 @@ public class PL2303SerialDevice extends UsbSerialDevice
 	private static final int PL2303_REQTYPE_HOST2DEVICE = 0x21;
 	
 	private static final int PL2303_VENDOR_WRITE_REQUEST = 0x01;
-	private static final int PL2303_SET_LINE_CODING = 0x21;
+	private static final int PL2303_SET_LINE_CODING = 0x20;
+	
+	private byte[] defaultSetLine = new byte[]{
+			(byte) 0x80, // [0:3] Baud rate (reverse hex encoding 9600:00 00 25 80 -> 80 25 00 00)
+			(byte) 0x25,
+			(byte) 0x00,
+			(byte) 0x00,
+			(byte) 0x00, // [4] Stop Bits (0=1, 1=1.5, 2=2)
+			(byte) 0x00, // [5] Parity (0=NONE 1=ODD 2=EVEN 3=MARK 4=SPACE)
+			(byte) 0x08  // [6] Data Bits (5=5, 6=6, 7=7, 8=8)
+	};
 	
 	
 	private UsbInterface mInterface;
@@ -64,7 +74,7 @@ public class PL2303SerialDevice extends UsbSerialDevice
 		
 		//Default Setup
 		byte[] buf = new byte[2];
-		//Specific vendor stuff that I barely understand but It is on linux drivers, So I trust :)
+			//Specific vendor stuff that I barely understand but It is on linux drivers, So I trust :)
 		setControlCommand(PL2303_REQTYPE_DEVICE2HOST_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x8484, 0, buf);
 		setControlCommand(PL2303_REQTYPE_HOST2DEVICE_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x0404, 0, buf);
 		setControlCommand(PL2303_REQTYPE_DEVICE2HOST_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x8484, 0, buf);
@@ -75,7 +85,8 @@ public class PL2303SerialDevice extends UsbSerialDevice
 		setControlCommand(PL2303_REQTYPE_DEVICE2HOST_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x8383, 0, buf);
 		setControlCommand(PL2303_REQTYPE_HOST2DEVICE_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x0000, 0, null);
 		setControlCommand(PL2303_REQTYPE_HOST2DEVICE_VENDOR, PL2303_VENDOR_WRITE_REQUEST, 0x0001, 0, null);
-		// End of specific vendor stuff
+			// End of specific vendor stuff
+		setControlCommand(PL2303_REQTYPE_HOST2DEVICE, PL2303_SET_LINE_CODING, 0x0000, 0, defaultSetLine);
 		
 		// Initialize UsbRequest
 		requestIN = new UsbRequest();
@@ -84,66 +95,61 @@ public class PL2303SerialDevice extends UsbSerialDevice
 		// Pass references to the threads
 		workerThread.setUsbRequest(requestIN);
 		writeThread.setUsbEndpoint(outEndpoint);
-
-
 	}
 
 	@Override
 	public void close() 
 	{
-		// TODO Auto-generated method stub
-		
+		killWorkingThread();
+		killWriteThread();
+		connection.close();
 	}
 
 	@Override
 	public void setBaudRate(int baudRate) 
 	{
-		// TODO Auto-generated method stub
+		defaultSetLine[0] = (byte) (baudRate & 0xff);
+		defaultSetLine[1] = (byte) (baudRate >> 8 & 0xff);
+		defaultSetLine[2] = (byte) (baudRate >> 16 & 0xff);
+		defaultSetLine[3] = (byte) (baudRate >> 24 & 0xff);
 		
+		setControlCommand(PL2303_REQTYPE_HOST2DEVICE, PL2303_SET_LINE_CODING, 0x0000, 0, defaultSetLine);
 	}
 
 	@Override
 	public void setDataBits(int dataBits) 
 	{
-		// TODO Auto-generated method stub
-		
+		defaultSetLine[6] = (byte) dataBits;
+		setControlCommand(PL2303_REQTYPE_HOST2DEVICE, PL2303_SET_LINE_CODING, 0x0000, 0, defaultSetLine);
 	}
 
 	@Override
 	public void setStopBits(int stopBits)
 	{
-		// TODO Auto-generated method stub
-		
+		defaultSetLine[4] = (byte) stopBits;
+		setControlCommand(PL2303_REQTYPE_HOST2DEVICE, PL2303_SET_LINE_CODING, 0x0000, 0, defaultSetLine);
 	}
 
 	@Override
 	public void setParity(int parity) 
 	{
-		// TODO Auto-generated method stub
-		
+		defaultSetLine[5] = (byte) parity;
+		setControlCommand(PL2303_REQTYPE_HOST2DEVICE, PL2303_SET_LINE_CODING, 0x0000, 0, defaultSetLine);	
 	}
 
 	@Override
 	public void setFlowControl(int flowControl)
 	{
-		// TODO Auto-generated method stub
+		// TODO
 		
 	}
 	
-	private byte[] encodeBaudRateDiv(int baudRate)
-	{
-		int tmp;
-		byte[] baudRateEncoded = new byte[4];
-		tmp = 12000000 * 32 / baudRate;
-	}
 	
 	private int setControlCommand(int reqType ,int request, int value, int index, byte[] data)
 	{
 		int dataLength = 0;
 		if(data != null)
-		{
 			dataLength = data.length;
-		}
 		int response = connection.controlTransfer(reqType, request, value, index, data, dataLength, USB_TIMEOUT);
 		Log.i(CLASS_ID,"Control Transfer Response: " + String.valueOf(response));
 		return response;
