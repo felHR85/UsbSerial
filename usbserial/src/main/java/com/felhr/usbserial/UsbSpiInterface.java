7cd6434ad140207f3bdbec07bbdc283e458b2f6c
Felipe Herranz <felhr85@gmail.com>
2016-04-23 13:45:51 +0200

    Added SPI interface

---
+++ usbserial/src/main/java/com/felhr/usbserial/UsbSpiInterface.java
@@ -0,0 +1,31 @@
+package com.felhr.usbserial;
+
+
+public interface UsbSpiInterface
+{
+    // Clock dividers;
+    int DIVIDER_2 = 2;
+    int DIVIDER_4 = 4;
+    int DIVIDER_8 = 8;
+    int DIVIDER_16 = 16;
+    int DIVIDER_32 = 32;
+    int DIVIDER_64 = 64;
+    int DIVIDER_128 = 128;
+
+    // Common SPI operations
+    void connectSPI();
+    void writeMOSI(byte[] buffer);
+    void readMISO(int lengthBuffer);
+    void setClock(int clockDivider);
+    void selectSlave(int nSlave);
+    void setMISOCallback(UsbMISOCallback misoCallback);
+
+    // Status information
+    int getClockDivider();
+    int getSelectedSlave();
+
+    interface UsbMISOCallback
+    {
+        int onReceivedData(byte[] data);
+    }
+}
