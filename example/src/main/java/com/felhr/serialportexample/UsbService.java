8bd88ea83b2b50db6106965f64fb6d977a038f01
Felipe Herranz <felhr85@gmail.com>
2016-07-03 16:50:29 +0200

    fix usb hub excluding logic

--- example/src/main/java/com/felhr/serialportexample/UsbService.java
+++ example/src/main/java/com/felhr/serialportexample/UsbService.java
@@ -180,7 +180,7 @@ public class UsbService extends Service {
                 int deviceVID = device.getVendorId();
                 int devicePID = device.getProductId();
 
-                if (deviceVID != 0x1d6b && (devicePID != 0x0001 || devicePID != 0x0002 || devicePID != 0x0003)) {
+                if (deviceVID != 0x1d6b && (devicePID != 0x0001 && devicePID != 0x0002 && devicePID != 0x0003)) {
                     // There is a device connected to our Android device. Try to open it as a Serial Port.
                     requestUserPermission();
                     keep = false;
