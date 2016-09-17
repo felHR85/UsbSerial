c2c853d6d635519cbab462ca4d4f93cc64d39b38
Felipe Herranz <felhr85@gmail.com>
2016-09-17 16:09:04 +0200

    added UsbService to sync example

---
+++ examplesync/src/main/java/com/felhr/serialportexamplesync/MainActivity.java
@@ -0,0 +1,9 @@
+package com.felhr.serialportexamplesync;
+
+import android.app.Activity;
+
+/**
+ * Created by Felipe Herranz(felhr85@gmail.com) on 17/9/16.
+ */
+public class MainActivity extends Activity{
+}
