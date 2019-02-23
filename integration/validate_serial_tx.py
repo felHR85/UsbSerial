ed8cf6208e9d088cc6b4696d31bc79ed47f5c484
Felipe Herranz <felhr85@gmail.com>
2019-02-23 23:49:26 +0100

    fix

--- integration/validate_serial_tx.py
+++ integration/validate_serial_tx.py
@@ -2,14 +2,22 @@ import serial
 import sys
 import os
 
-port = sys.argv[0] 
+port = sys.argv[0]
 size = sys.argv[1]
 speed = sys.argv[2]
 
-comm = serial.Serial()
-comm.
+comm = serial.Serial(port, int(speed))
 
-data = os.urandom(size)
+data_tx = os.urandom(size)
+
+comm.write(data_tx)
+
+data_rx = comm.read(size)
+
+if data_tx == data_rx:
+    print("Success: Data was transmitted correctly")
+else:
+    print("Error: Data was not transmitted")
 
 
 
